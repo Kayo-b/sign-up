2ffabad58bb6fb682f94da9bd87e3b84616f3408
kayo-b <kyo.bhz@gmail.com>
2022-03-20 11:22:39 -0300

    styling remake

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const submit = document.getElementById("create")
 submit.onmousedown = function(){
     // submit.style.borderColor = "gray";
-    submit.style.border = "2px solid gray"
+    submit.style.border = "1px solid rgb(15, 0, 151)"
     submit.onmouseup = function (){
         // submit.style.borderColor = "rgb(57, 51, 109)"
-        submit.style.border = "2px solid rgb(57, 51, 109)"
+        submit.style.border = "1px solid rgb(57, 51, 109)"
     }
 }
 
